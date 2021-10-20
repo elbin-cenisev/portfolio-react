af284d5fa562109063adc1ad36c5184d24b69c73
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-20 17:32:19 -0400

    Styled cover and added intro

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-import {  } from 'react-bootstrap';
+import { } from 'react-bootstrap';
 
 // Navbar and "Brand"
 function Cover() {
     return (
-        <p>Hello World</p>
+        <main className="px-3">
+            <h1>Hi, I'm Elbin</h1>
+            <p className="lead">I am currently learning to become a full-stack web developer.</p>
+            <p className="lead">This portfolio page contains some examples of my work, as well as my contact information.</p>
+        </main>
     );
 }
 
