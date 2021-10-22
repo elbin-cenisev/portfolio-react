b1da51b86533db9722629161e3e66e189acc56a3
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-22 15:48:21 -0400

    Added picture of myself.........

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
-import { } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
+import me from '../images/me-bw.png'
 
 // Navbar and "Brand"
 function Cover() {
     return (
         <main className="px-3">
-            <h1>Hello there!</h1>
-            <p className="lead">I am currently learning to become a full-stack web developer. This portfolio page contains some examples of my work, as well as my contact information.</p>
+            <Image src={me} />
+            <h1 className="my-3">Hello there!</h1>
+            <p className="lead">My name is Elbin and I am an up-and-coming (hopefully) web developer!</p>
+            <p className="lead">This portfolio page contains some examples of my work, as well as my contact information.</p>
         </main>
     );
 }
