900896c9df6436859e1d610a3b7bf0983214bb4c
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-22 17:46:14 -0400

    Added form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { Container, Image, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Image, ListGroup, Form, Button } from 'react-bootstrap';
 import linkedInLogo from '../images/linkedInLogo.png'
-import emailLogo from '../images/outlookLogo.png'
 import githubLogo from '../images/githubLogo.png'
 
 // Navbar and "Brand"
@@ -10,13 +9,31 @@ function Contact() {
     return (
         <Container className="contact">
             <h1 className="mt-3">Contact Me</h1>
-            <p className="lead">You can use any of the following methods to contact me!</p>
+            <p className="lead">You can use this form to send me a message</p>
+
+                <Row className="justify-content-center">
+                        <Form className="text-center" style={{ width: '50%' }}>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                </Row>
+
+            <p className="lead mt-5">You can also check out my social media accounts using these links</p>
             <ListGroup className="w-50 mx-auto">
                 <ListGroup.Item action href="https://www.linkedin.com/in/elbin-cenisev-7a31568a/" className="mx-auto listItem">
                     <Image width="40px" height="40px" src={linkedInLogo} /> LinkedIn
-                </ListGroup.Item>
-                <ListGroup.Item action href="mailto:elbincenisev@outlook.com" className="mx-auto listItem">
-                    <Image width="40px" height="40px" src={emailLogo} /> Email
                 </ListGroup.Item>
                 <ListGroup.Item action href="https://github.com/elbin-cenisev" className="mx-auto listItem">
                     <Image width="40px" height="40px" src={githubLogo} />GitHub
