de9f433025742040d9004c27188f9edc714138e5
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-22 21:34:38 -0400

    Added name, email and message form inputs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,6 @@
 import React from 'react';
-
-import { Container, Row, Col, Image, ListGroup, Form, Button } from 'react-bootstrap';
-import linkedInLogo from '../images/linkedInLogo.png'
-import githubLogo from '../images/githubLogo.png'
+var validator = require('validator');
+import { Container, Row, Form, Button } from 'react-bootstrap';
 
 // Navbar and "Brand"
 function Contact() {
@@ -13,32 +11,28 @@ function Contact() {
 
                 <Row className="justify-content-center">
                         <Form className="text-center" style={{ width: '50%' }}>
-                            <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                        <Form.Group className="mb-3" controlId="formName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter your name" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formEmail">
                                 <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
+                                <Form.Control type="email" placeholder="Enter your email" />
                             </Form.Group>
-                            <Form.Group className="mb-3" controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+
+                            <Form.Group className="mb-3" controlId="formMessage">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control as="textarea" type="text" 
+                                style={{ height: '100px' }}placeholder="Enter your message" />
                             </Form.Group>
+
                             <Button variant="primary" type="submit">
                                 Submit
                             </Button>
                         </Form>
                 </Row>
-
-            <p className="lead mt-5">You can also check out my social media accounts using these links</p>
-            <ListGroup className="w-50 mx-auto">
-                <ListGroup.Item action href="https://www.linkedin.com/in/elbin-cenisev-7a31568a/" className="mx-auto listItem">
-                    <Image width="40px" height="40px" src={linkedInLogo} /> LinkedIn
-                </ListGroup.Item>
-                <ListGroup.Item action href="https://github.com/elbin-cenisev" className="mx-auto listItem">
-                    <Image width="40px" height="40px" src={githubLogo} />GitHub
-                </ListGroup.Item>
-            </ListGroup>
         </Container>
     );
 }
